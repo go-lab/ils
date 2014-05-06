da50d069fa71c1761b6232d1080a0c58dd571334
Wissam-Halimi <wissam.halimi@epfl.ch>
2014-05-06 15:32:53 +0200

    Error handling for some functions done

--- main/ils.js
+++ main/ils.js
@@ -38,10 +38,11 @@ ILS Library for Go-Lab
       }
     },
     createResource: function(resourceName, content, cb) {
-      ils.getVault(function(vault) {
-        ils.getCurrentUser(function(username){
-          var params = {
-            "document": {
+      if(resourceName != null && resourceName != undefined){
+        ils.getVault(function(vault) {
+          ils.getCurrentUser(function(username){
+            var params = {
+              "document": {
               "parentType": "@space",
               "parentId": vault.id,
               "displayName": resourceName,
@@ -55,7 +56,10 @@ ILS Library for Go-Lab
             cb(resource.entry);
           });
         });
-      });
+        });}else{
+          var error = {"error" : "resourceName cannot be null. Cannot create resource."};
+          return cb(error);
+        }
     },
     listVault: function(cb) {
       ils.getVault(function(vault) {
@@ -65,6 +69,7 @@ ILS Library for Go-Lab
       });
     },
     getIls: function(cb) {
+      var error = {"error" : "Cannot get ILS"};
       osapi.context.get().execute(function(space) {
         if (!space.error) {
           console.log("print context");
@@ -74,38 +79,57 @@ ILS Library for Go-Lab
               console.log("print parent space");
               console.log(parentSpace);
               osapi.spaces.get({contextId: parentSpace.parentId}).execute(function (parentIls){
-                if(!parentIls.error){
+                if(!parentIls.error && parentIls.spacetype == 'ils'){
                   console.log("print ils space");
                   console.log(parentIls);
                   return cb(parentIls);
+                }else{
+                  return cb(error);
                 }
               });
+            }else{
+              return cb(error);
             }
           });
+        }else{
+          return cb(error);
         }
       });
     },
     getVault: function(cb) {
-      return ils.getIls(function(parentIls) {
+      var error = '';
+      ils.getIls(function(parentIls) {
+        if(!parentIls.error){
         console.log(parentIls.id);
-        return osapi.spaces.get({contextId: parentIls.id, contextType: "@space"}).execute(
+        osapi.spaces.get({contextId: parentIls.id, contextType: "@space"}).execute(
           function(subspaces) {
-            var item, vault;
-            vault = (function() {
-              var i, len, ref, results;
-              ref = subspaces.list;
-              results = [];
-              for (i = 0, len = ref.length; i < len; i++) {
-                item = ref[i];
-                if (JSON.parse(item.metadata).type === "Vault") {
-                  results.push(item);
+            if(subspaces.totalResults != 0){
+              var item, vault;
+              vault = (function() {
+                var i, len, ref, results;
+                ref = subspaces.list;
+                results = [];
+                for (i = 0, len = ref.length; i < len; i++) {
+                  item = ref[i];
+                  if(JSON.parse(item.metadata) != null && JSON.parse(item.metadata) != undefined){
+                    if (JSON.parse(item.metadata).type === "Vault") {
+                      results.push(item);
+                  }
                 }
               }
               return results;
             })();
             return cb(vault[0]);
-          });
-      });
+          }else{
+            error = {"error" : "No subspaces in current ILS"};
+            return cb(error);
+          }
+        });
+      }else{
+        error = {"error" : "Cannot get the vault"};
+        return cb(error);
+      }
+    });
     },
     getParentInquiryPhase: function(cb) {
       return this.getParent(function(parent) {
